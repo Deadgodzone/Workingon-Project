39fca5ddf1124b8a35072a96adbeb7ed6bb933cc
Walid Nawfal Sabihi <walidopenrealms@gmail.com>
2015-04-06 16:27:16 +0100

    Implemented SHA1 on password login check
    The password that is sent to login page specified by you is now already
    in SHA1, please remove any SHA1 function for the password received in
    your login script if you have it.

--- WhiteRealmsAdminDashboard/WhiteRealmsAdminDashboard/CoreClass.cs
+++ WhiteRealmsAdminDashboard/WhiteRealmsAdminDashboard/CoreClass.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Net.Http;
 using System.Web;
 using System.Net;
+using System.Security.Cryptography;
 
 namespace WhiteRealmsAdminDashboard
 {
@@ -18,8 +19,25 @@ namespace WhiteRealmsAdminDashboard
         public static void TryLogin(string username, string password)
         {
             var client = new WebClient();
-            string html = client.DownloadString(adminLoginPage + "?mail=" + username + "&password_hash=" + password);
+            string html = client.DownloadString(adminLoginPage + "?mail=" + username + "&password_hash=" + Hash(password));
             Boolean.TryParse(html,out isLogin);
+        }
+
+        static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
         }
     }
 }
